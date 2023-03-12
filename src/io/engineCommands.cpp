fe5699ddc89cc8f8e58101f76d6006a3fa47fbf7
Philipp Geil <Philipp-Geil@outlook.com>
2023-03-12 23:37:14 +0100

    Add list variables command

--- src/io/engineCommands.cpp
+++ src/io/engineCommands.cpp
@@ -3,6 +3,9 @@
 #include "io/ioStream.hpp"
 #include <mathlib/mathlib.hpp>
 
+#include <iomanip>
+#include <sstream>
+
 namespace cas {
     void Engine::setupCommands() {
         CommandCallback<std::string> printStr = [&](std::string str) {
@@ -14,15 +17,36 @@ namespace cas {
 
             return "shutting down!";
         });
+        addCommand("exit", exitCommand, printStr);
 
+        // TODO: Add short form x=expr; == set[x,expr];
         Command<math::Expression*, math::Variable*, math::Expression*> setCommand = Command<math::Expression*, math::Variable*, math::Expression*>(
             [](Engine* engine, math::Variable* var, math::Expression* expr) {
                 engine->vars[var->getSymbol()] = expr;
 
                 return expr;
-        });        
-
-        addCommand("exit", exitCommand, printStr);
+            });
         addCommand("set", setCommand);
+
+        Command<std::string> listVarsCommand = Command<std::string>(
+            [](Engine* engine) {
+                std::stringstream ss;
+
+                const char separator = ' ';
+                const int symbolWidth = 10;
+
+                ss << std::left << std::setw(symbolWidth) << std::setfill(separator) << "Variable"
+                   << " | "
+                   << "Value";
+
+                for (const auto& [symbol, expr] : engine->vars) {
+                    ss << std::endl;
+                    ss << std::left << std::setw(symbolWidth) << std::setfill(separator) << symbol << " | ";
+                    ss << expr->toString();
+                }
+
+                return ss.str();
+            });
+        addCommand("listVars", listVarsCommand, printStr);
     }
 } // namespace cas
