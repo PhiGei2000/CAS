f1e14b5167292b0421e99fa873e2ec187037b3e4
Philipp Geil <Philipp-Geil@outlook.com>
2023-03-14 14:05:09 +0100

    Change function class implementation to header only
    Avoid explicit template instantiations for the number of arguments

--- mathlib/include/expressions/functions/function.hpp
+++ mathlib/include/expressions/functions/function.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "../terms/expression.hpp"
+#include "../terms/multiplication.hpp"
+#include "../terms/variable.hpp"
+
+#include <sstream>
 
 namespace cas::math {
     struct BaseFunction : public Expression {
@@ -17,12 +21,51 @@ namespace cas::math {
       public:
         Expression* arguments[u];
 
-        virtual bool dependsOn(const Variable& var) const override;
+        virtual std::vector<Expression*> getChildren() const override {
+            return std::vector<Expression*>(std::begin(arguments), std::end(arguments));
+        }
 
-        virtual std::string toString() const override;
+        virtual bool dependsOn(const Variable& var) const override {
+            for (const Expression* arg : arguments) {
+                if (arg->dependsOn(var)) {
+                    return true;
+                }
+            }
 
-        virtual std::set<Variable> getVariables() const override;
+            return false;
+        }
 
-        virtual Expression* differentiate(const Variable* var) const override;
+        virtual std::string toString() const override {
+            std::stringstream result;
+            result << name << "(" << arguments[0]->toString();
+
+            for (int i = 1; i < u; i++) {
+                result << ", " << arguments[i]->toString();
+            }
+
+            result << ")";
+            return result.str();
+        }
+
+        virtual std::set<Variable> getVariables() const override {
+            std::set<Variable> vars = arguments[0]->getVariables();
+            for (int i = 1; i < u; i++) {
+                vars.merge(arguments[i]->getVariables());
+            }
+
+            return vars;
+        }
+
+        virtual Expression* differentiate(const Variable* var) const override {
+            Expression* result = getDerivative();
+
+            for (int i = 0; i < u; i++) {
+                Expression* dArg = arguments[i]->differentiate(var);
+                Multiplication* prod = new Multiplication(result, dArg);
+                result = prod;
+            }
+
+            return result;
+        }
     };
 } // namespace cas::math
